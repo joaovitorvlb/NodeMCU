8cf21e109a91703e6cf99d4f2da6a7977dd18b2c
joao <joaovitorvlb@hotmail.com>
2017-07-30 21:37:32 -0300

    add WiFiSocketClient/

---
+++ WiFiSocketClient/WiFiSocketClient.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "IOT-2";
+const char* password = "mj110032";
+
+const char* host = "192.168.0.12";
+const int httpPort = 9765;
+// Use WiFiClient class to create TCP connections
+WiFiClient client;
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("conectando no ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");  
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+
+  delay(500);
+
+  Serial.print("Conectando ao ");
+  Serial.println(host);
+  
+  while (client.connect(host, httpPort) != 1) 
+    {
+      delay(500);
+      Serial.print(":");
+    }
+  Serial.println("Conectado ao servidor");
+  delay(500);
+}
+
+
+
+void loop() 
+{
+  if (client.available()) 
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+  
+  if (client.connected() == 0) 
+  {
+    while (client.connect(host, httpPort) == 0) 
+    {
+      delay(500);
+      Serial.print(":");
+    }
+    Serial.println();
+    Serial.println("Conectado ao servidor");
+  }
+}
+
